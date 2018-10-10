963861f502bd3b9b85f1089d9c81ed0125077709
John Hayes <johnnyhayes7@gmail.com>
2018-10-10 18:19:52 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+function paragraphSelector(){
+ var p = $('p');
+ return p;
+}
+
+function lastImageSelector(){
+  var last = $('img:last');
+  return last;
+}
+
+function ninjaBabySelector(){
+  var ninja = $('#baby-ninja');
+  return ninja;
+}
+
+function divSelector(){
+  var divs = $('.pics');
+  return divs;
+}
+
